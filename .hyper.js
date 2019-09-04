f80c3690d8c25b79de9ff9f39916b86f075659ea
Rich Posert <rich@posert.com>
2019-09-04 15:21:22 -0700

    right click to paste in hyper

--- .hyper.js
+++ .hyper.js
@@ -124,7 +124,7 @@ module.exports = {
 
     // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
     // selection is present (`true` by default on Windows and disables the context menu feature)
-    // quickEdit: true,
+    quickEdit: true,
 
     // URL to custom bell
     // bellSoundURL: 'http://example.com/bell.mp3',
