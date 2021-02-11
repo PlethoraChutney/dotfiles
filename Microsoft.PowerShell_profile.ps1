223f85742056133a4a2d40e09104907044fdce67
Rich <rich@posert.com>
2021-02-11 12:30:33 -0800

    Powershell prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 # powershell environment preferences
 
+# Custom prompt
 Set-PsReadLineOption -Colors @{
     'ForegroundColor' = [ConsoleColor]::Green
     'String' = [ConsoleColor]::Blue
@@ -9,6 +10,26 @@ Set-PSReadlineOption -BellStyle None
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 $Host.PrivateData.ProgressForegroundColor = 'Green'
 $Host.PrivateData.ProgressBackgroundColor = 'Black'
+
+function prompt {
+
+    #Assign Windows Title Text
+    $host.ui.RawUI.WindowTitle = "Current Folder: $pwd"
+
+    #Configure current user, current folder and date outputs
+    $CmdPromptUser = [Security.Principal.WindowsIdentity]::GetCurrent();
+
+    # Test for Admin / Elevated
+    $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+    #Decorate the CMD Prompt
+    Write-host ($(if ($IsAdmin) { 'Elevated ' } else { '' })) -ForegroundColor Red -NoNewline
+    Write-Host "$($CmdPromptUser.Name.split("\")[1]) " -ForegroundColor Green -NoNewline
+    Write-Host "@ " -ForegroundColor White -NoNewline
+    Write-Host "Powershell " -NoNewline -ForegroundColor Red
+    Write-Host $pwd  -ForegroundColor Blue
+    return "> "
+} #end prompt function
 
 # directory traversing
 $DOCDIR = [Environment]::GetFolderPath("MyDocuments")
