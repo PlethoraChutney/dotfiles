ee65ecf752578769f87833b02cca91ced5853339
Rich <rich@posert.com>
2024-12-19 14:57:11 -0800

    Rust changes

--- chimerax-config/rotate-sep.py
+++ chimerax-config/rotate-sep.py
@@ -1,5 +1,19 @@
-def rotate_separately(session, models, axis, degrees, frames):
+def rotate_separately(session, axis, degrees, frames, models = None):
+    """
+    Rotate models independently (rather than rotating the whole scene).
+
+    Parameters
+    ----------
+    axis: Scene axis about which to rotate
+    degrees: Degrees per frame to rotate
+    frames: Number of frames over which to rotate
+    models: (optional) Models to rotate. If this argument is not provided,
+        all models are rotated.
+    """
     from chimerax.core.commands import run
+
+    if models is None:
+        models = session.models
     base_ids = set(m.id[0] for m in models)
     for m_id in base_ids:
         run(session, f"turn {axis} {degrees} {frames} models #{m_id} center #{m_id}")
@@ -9,13 +23,15 @@ def register_command(logger):
 
     desc = CmdDesc(
         required = [
-            ("models", ModelsArg),
             ("axis", StringArg),
             ("degrees", FloatArg),
             ("frames", IntArg)
         ],
+        optional = [
+            ("models", ModelsArg)
+        ],
         synopsis = "Rotate maps independently"
     )
-    register("iturn_all", desc, rotate_separately, logger = logger)
+    register("rot_all", desc, rotate_separately, logger = logger)
 
 register_command(session.logger)
