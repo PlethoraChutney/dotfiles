df83555aaf7818e7c6758f34b8748b693e970143
Rich <rich@posert.com>
2025-01-08 08:58:20 -0800

    Allow for adjusting framerate of classiter

--- chimerax-config/python-scripts/class-iter.py
+++ chimerax-config/python-scripts/class-iter.py
@@ -1,18 +1,21 @@
-def classiter(session, vser, label_text):
+def classiter(session, vser, label_text, frameduration = 10):
     from chimerax.core.commands import run
     root_model = list(vser)[0]
     vols = root_model.child_models()
-    run(session, f"perframe \"vser play #{root_model.id[0]} jump $1; 2dlab delete; 2dlab text '{label_text} $1' xpos 0.025 ypos 0.95 font Inter\" range 0,{len(vols) - 1} frames {len(vols)} interval 10")
+    run(session, f"perframe \"vser play #{root_model.id[0]} jump $1; 2dlab delete; 2dlab text '{label_text} $1' xpos 0.025 ypos 0.95 font Inter\" range 0,{len(vols) - 1} frames {len(vols)} interval {frameduration}")
 
 def register_command(logger):
-    from chimerax.core.commands import register, CmdDesc, StringArg, FloatArg, ModelsArg, IntArg
+    from chimerax.core.commands import register, CmdDesc, StringArg, ModelsArg, IntArg
 
     desc = CmdDesc(
         required = [
             ("vser", ModelsArg),
             ("label_text", StringArg),
         ],
-        synopsis = "Rotate maps independently"
+        optional = [
+            ("frameduration", IntArg)
+        ],
+        synopsis = "Display and label a volume series. Each volume is shown for {frameduration} frames."
     )
 
     register("classiter", desc, classiter, logger = logger)
