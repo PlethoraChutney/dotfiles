bef1efd83a1cdc16609b1fb7c78c854d4c85e815
Rich <rich@posert.com>
2025-07-25 07:57:25 -0700

    Helix hidden files

--- helix/provision.sh
+++ helix/provision.sh
@@ -1,6 +1,15 @@
 #!/bin/zsh
 set -e
 
+if (( $+commands[brew] )); then
+  brew install helix ruff
+else
+  echo "Manually install helix"
+  mamba activate base
+  mamba install ruff
+  ln -s $(command -v ruff) ~/.local/bin/
+done
+npm install -g bash-language-server
 npm install -g @olrtg/emmet-language-server
 cargo install --git https://github.com/estin/simple-completion-language-server.git
 
