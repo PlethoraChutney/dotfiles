6a2c46fdc7dae2f146ceb3e5a6c3bb612f44bccd
Rich <rich@posert.com>
2025-07-31 13:17:30 -0700

    Helix: lots of reveal stuff

--- helix/provision.sh
+++ helix/provision.sh
@@ -9,9 +9,12 @@ else
   echo "Manually install helix"
   mamba activate base
   mamba install ruff
+  pipx install jedi
   ln -s $(command -v ruff) ~/.local/bin/
 npm install -g bash-language-server
 npm install -g @olrtg/emmet-language-server
+npm install -g typescript typescript-language-server
+npm install -g vscode-langservers-extracted
 cargo install --git https://github.com/estin/simple-completion-language-server.git
 cargo install codebook-lsp
 
